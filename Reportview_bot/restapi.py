f0c1ca5e74861ce1e79124dbfba1470e2274b5b9
KimNayoun <94847818+NayounK1m@users.noreply.github.com>
2022-06-23 18:46:02 +0900

    코드정리
    2022-06-23

--- Reportview_bot/restapi.py
+++ Reportview_bot/restapi.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 api = Api(app)
 class CreateUser(Resource):
     def post(self):
-        try:#보내는 코드
+        try:
+            #입력 파라미터를 설정
             parser = reqparse.RequestParser()
+            #필수 매개변수
             parser.add_argument('tent_1', type=str)
             parser.add_argument('tent_2', type=str)
             parser.add_argument('tent_3', type=str)
             parser.add_argument('tent_4', type=str)
-            args = parser.parse_args()
-            #받는 코드
+            #입력 파싱
+            args = parser.parse_args() 
             _tent_1 = args['tent_1']
             _tent_2 = args['tent_2']
             _tent_3 = args['tent_3']
@@ -30,13 +32,18 @@ class CreateUser(Resource):
             f.write(content['tent_4']+'\n')
             
             f.close()
-            
-            return {'StatusCode': '200', 'Message': '저장 성공!'}    
+
+            #성공값 출력
+            return {'StatusCode': '200', 'Message': '저장 성공!'}
+
+        #에러메세지 출력
         except Exception as e:
             return {'error': str(e)}
 
+#http://localhost:8080/report
 api.add_resource(CreateUser, '/report')
 
+#flask ip, port 설정
 if __name__ ==  '__main__':
     app.run(
     host="localhost",
